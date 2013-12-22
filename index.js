3c0f011db2cb1692774609272241d14e9273157a
Adam Duncan <adam.duncan@clock.co.uk>
2013-12-22 19:54:55 +0000

    Changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || DEFAULT_PORT;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello Adam\n');
+  res.send('Hello Adam!!\n');
 });
 
 app.listen(PORT)
