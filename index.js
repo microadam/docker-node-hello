b4f6fc8c8e209e1c3a2d1d3a993ad28ff1d17828
Adam Duncan <adam.duncan@clock.co.uk>
2013-12-30 00:07:11 +0000

    Add graceful shutdown

--- index.js
+++ index.js
@@ -5,10 +5,37 @@ var DEFAULT_PORT = 6500;
 var PORT = process.env.PORT || DEFAULT_PORT;
 
 // App
-var app = express();
+var app = express()
+  , gracefullyExiting = false
+
+// Return 502 while the server is shutting down
+app.use(function (req, res, next) {
+  if (!gracefullyExiting) {
+    return next()
+  }
+  res.set('Connection', 'close')
+  res.send(502, 'Server is in the process of restarting.')
+})
+
 app.get('/', function (req, res) {
   res.send('Hello Adam\n');
 });
 
-app.listen(PORT)
+var server = app.listen(PORT)
 console.log('Running on http://localhost:' + PORT);
+
+// Graceful shutdown taken from: http://blog.argteam.com/
+process.on('SIGTERM', function () {
+  gracefullyExiting = true
+  console.log('Received kill signal (SIGTERM), shutting down')
+
+  setTimeout(function () {
+    console.log('Could not close connections in time, forcefully shutting down')
+    process.exit(1)
+  }, 30000)
+
+  server.close(function () {
+    console.log('Closed out remaining connections.')
+    process.exit()
+  })
+})
